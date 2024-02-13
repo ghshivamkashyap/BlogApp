d2acd3acc68b5b9cb0a6259e5511cdb521a57e13
SuhaniKapasiya <107610361+SuhaniKapasiya@users.noreply.github.com>
2024-02-14 00:11:17 +0530

    update

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+
+//middleware
+app.use(express.json());
+
+const blog = require("./routes/blog")
+app.use("/api/v1",blog);
+
+const connectWithDB = require("./config/database");
+connectWithDB();
+
+app.listen(PORT, ()=>{
+    console.log(`App is started at Port no ${PORT}`);
+
+})
+
+app.get("/", (req, res)=>{
+    res.send(`<h1>This is MY HomePagesaaaa</h1>`)
+})
