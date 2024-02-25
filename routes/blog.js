5827f647ec78168d0601c1a4beb823229c15fb20
SHIVAM KASHYAP <2130103@sliet.ac.in>
2024-02-26 00:48:13 +0530

    post corection

--- routes/blog.js
+++ routes/blog.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const { createComments } = require("../controllers/commentsController");
+const { createPost } = require("../controllers/postController");
 const router = express.Router();
 
 
 //Import Controller
-const {createComments} = require("../controllers/CommentsController")
+// const {createComments} = require("../controllers/CommentsController")
 
 
 
 //Mapping Create 
 router.post("/comments/create", createComments);
+router.post("/post/create", createPost);
 
 
 //export
